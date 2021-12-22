f046d9365cccc5bbb2af5c7f61600f2d9910f6cb
anemadhon <anemadhon@gmail.com>
2021-12-22 22:37:08 +0700

    refactor article request

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -25,36 +25,40 @@ class ArticleRequest extends FormRequest
      */
     public function rules()
     {
-        $max_images = ArticleImage::MAX_IMAGES - ($this->article ? $this->article->images->count() : 0);
-        $max_image_rules = ($max_images < 1 || $this->image_flag === 'edit') ? '' : "max:{$max_images}";
-
-        $max_tags = Tag::MAX_TAGS - ($this->article ? $this->article->tags->count() : 0);
-        $max_tag_rules = $max_tags < 1 ? '' : "max:{$max_tags}";
-
         return [
             'title' => ['required', 'string', 'min:4', 'max:255'],
             'content' => ['required', 'string', 'min:2'],
             'category_id' => ['required', 'integer', 'exists:categories,id'],
-            'tags' => ['required', 'min:1', $max_tag_rules],
+            'tags' => ['required', 'min:1', $this->max_tag_rules],
             'tags.*' => ['required', 'integer', 'exists:tags,id'],
-            'path' => [$max_image_rules],
+            'path' => [$this->max_image_rules],
             'path.*' => ['image', 'mimes:png,jpg,jpeg', 'max:2048']
         ];
     }
 
     public function messages()
     {
-        $max_images = ArticleImage::MAX_IMAGES - ($this->article ? $this->article->images->count() : 0);
-        $max_tags = Tag::MAX_TAGS - ($this->article ? $this->article->tags->count() : 0);
-
         return [
             'category_id.required' => 'Please select 1 Category',
             'category_id.exists' => 'You selected The Wrong Category',
             'tags.required' => 'Please choose at least 1 Tag',
             'tags.*.required' => 'Please choose at least 1 Tag',
             'tags.*.exists' => 'You choosed The Wrong Tag',
-            'tags.max' => "You've reached the maximum of the tagged articles ({$max_tags})",
-            'path.max' => "You've reached the maximum of the attached file ({$max_images})"
+            'tags.max' => "You've reached the maximum of the tagged articles ({$this->max_tags})",
+            'path.max' => "You've reached the maximum of the attached file ({$this->max_images})"
         ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $max_tags = Tag::MAX_TAGS - ($this->article ? $this->article->tags->count() : 0);
+        $max_images = ArticleImage::MAX_IMAGES - ($this->article ? $this->article->images->count() : 0);
+
+        $this->merge([
+            'max_tags' => $max_tags,
+            'max_tag_rules' => $max_tags < 1 ? '' : "max:{$max_tags}",
+            'max_images' => $max_images,
+            'max_image_rules' => ($max_images < 1 || $this->image_flag === 'edit') ? '' : "max:{$max_images}"
+        ]);
     }
 }
